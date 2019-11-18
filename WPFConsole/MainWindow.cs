a06d70ef24b6e35b6700149cdc9e616eeadc5d80
Vlad <Vlad@DESKTOP-HJ9OVGE>
2019-11-18 22:07:50 +0200

    Initilize

---
+++ WPFConsole/MainWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFConsole
+{
+    class MainWindow : Window
+    {
+        public MainWindow(string windowTitle, int heigth, int width)
+        {
+            this.Title = windowTitle;
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.Height = heigth;
+            this.Width = width;
+        }
+    }
+}
---
+++ WPFConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFConsole
+{
+    class Program : Application
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+            app.Startup += App_Startup;
+            app.Run();
+        }
+
+        private static void App_Startup(object sender, StartupEventArgs e)
+        {
+            var main = new MainWindow("Привіт бобер!", 200, 300);
+            main.Show();
+        }
+    }
+}
